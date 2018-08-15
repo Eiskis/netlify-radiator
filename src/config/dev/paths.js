1b730e6a4521d60d65341ececa69cc4757837e01
Jerry Jäppinen <eiskis@gmail.com>
2018-08-15 11:43:25 +0200

    Docs

--- src/config/dev/paths.js
+++ src/config/dev/paths.js
@@ -2,7 +2,13 @@ const merge = require('lodash').merge
 const baseConfig = require('../paths')
 
 module.exports = merge({}, baseConfig, {
+
+	// Use something like this for your local lambda server
 	functions: 'http://localhost:9000/',
+
+	// Use something like this for your deployed radiator
+	// functions: 'https://your-site-830jdj.netlify.com/.netlify/functions/',
+
 	host: 'http://localhost:8080/',
 	staticAssetsPath: '/static/'
 })
